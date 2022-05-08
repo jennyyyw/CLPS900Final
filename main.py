5dded744eff141d53f988971e1f0178782330c55
Jenny Wang <ze_hua_chen@brown.edu>
2022-05-08 08:25:22 +0800

    feat: added player and movement

--- main.py
+++ main.py
@@ -15,6 +15,51 @@ tile_dim = 50
 
 background = pygame.image.load('background.png')
 
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('player.png')
+        self.image = pygame.transform.scale(img, (30, 60))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+        dx = 0
+        dy = 0
+
+        #get keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -10
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 1
+        if key[pygame.K_RIGHT]:
+            dx += 1
+
+
+        #add gravity
+        self.vel_y += 0.5
+        if self.vel_y > 1:
+            self.vel_y = 1
+        dy += self.vel_y
+
+        #check for collision
+
+        #update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+            dy = 0
+
+        #draw player onto screen
+        window.blit(self.image, self.rect)
 
 def make_grid():
     for line in range(0, 10):
@@ -56,18 +101,19 @@ class Map():
 
 
 map_val = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 2, 2, 2, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 2, 0, 2, 2, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 2, 2, 0, 0, 1],
-    [1, 0, 0, 2, 2, 0, 0, 0, 0, 1],
-    [1, 2, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 2, 2, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 2, 0, 0, 0],
+    [0, 0, 0, 2, 2, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+player = Player(100, height - 130)
 env = Map(map_val)
 
 run = True
@@ -75,9 +121,11 @@ while run:
 
     window.blit(background, (0, 0))
     env.draw()
+    player.update()
     make_grid()
 
     pygame.display.update()
+
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
